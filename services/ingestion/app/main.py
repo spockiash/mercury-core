b801e5f1b26d804a623378f69bbbe095c1a10d7c
Bobo <spocky@post.cz>
2025-04-01 18:13:48 +0200

    created minimal debuggable setup with python DAP and nvim

--- services/ingestion/app/main.py
+++ services/ingestion/app/main.py
@@ -1,7 +1,13 @@
 # services/ingestion/app/main.py
-
+import debugpy
 from fastapi import FastAPI
 from api.routes import router as api_router
+
+debugpy.listen(("0.0.0.0", 5678))
+debugpy.wait_for_client()
+
+message = "I have intercepted this value from DAP"
+print(message)
 
 app = FastAPI(title="Mercury Ingestion Service")
 
